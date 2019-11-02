761c2241a476642f500c2c49f5c91f38e19e9e89
Larissa Magistrali <larissa.magistrali@acad.pucrs.br>
2019-11-02 12:27:40 -0300

    sinais de controle

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,10 +1,18 @@
 package app;
 
+import controller.SinaisDeControle;
+
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		SinaisDeControle controle = SinaisDeControle.getInstance();
 
 	}
+	
+	public void LeituraArquivo() {
+		
+	}
 
+	
 }
