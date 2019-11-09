a90ade02c7c05fd2c403f131297d79cb70465aff
nikolaslacerda <nikolaslacerda55@gmail.com>
2019-11-09 17:08:41 -0300

    Refatorando memória de dados

--- src/main/java/controller/MemoriaDeDados.java
+++ src/main/java/controller/MemoriaDeDados.java
@@ -11,9 +11,6 @@ public class MemoriaDeDados {
     //Tamanho da memoria
     private static int SIZE = 100000;
 
-    //Posicao do ultimo dado
-    private int posAtual = 0;
-
     //Construtor
     private MemoriaDeDados() {
         memoria = new String[SIZE];
@@ -26,28 +23,33 @@ public class MemoriaDeDados {
         return instance;
     }
 
-    //Adiciona um dado na memoria
-    public void addDado(String dado){
-        this.memoria[posAtual] = dado;
-        posAtual++;
-    }
-
-    //Recupera um dado da memoria
-    public String getDado(int pos){
+    //Recupera um dado da memoria dada uma posicao
+    public String readDado(int pos){
+        pos = (pos / 4); // Como a memoria do mips e de 4 em 4 temos que dividir por 4 para obter o valor equivalente no nosso array de 1 em 1
+        if(this.memoria[pos] == null){ // Endereco de memoria vazio, logo retorna 0 em 32 bits
+            return "00000000000000000000000000000000";
+        }
         return this.memoria[pos];
     }
 
-    //Posicao que esta o ultimo dado
-    public int getPosAtual(){
-        return this.posAtual;
+    //Adiciona um dado na memoria dada uma posicao
+    public void writeDado(String dado, int pos){
+        pos = (pos / 4);
+        if(pos > SIZE){
+            throw new ArrayIndexOutOfBoundsException("Overflow de memoria");
+        }
+        this.memoria[pos] = dado;
     }
 
     //Imprime
     public void imprime(){
         System.out.println("### Memoria de Dados ###");
-        for (int i = 0; i < posAtual; i++) {
-            System.out.println(memoria[i]);
-            System.out.println("DDD");
+        for (int i = 0; i < SIZE; i++) {
+            if(memoria[i] != null) {
+                System.out.println(i + " = " + memoria[i]);
+            }else {
+                System.out.println(i + " = " + "00000000000000000000000000000000");
+            }
         }
     }
 }
