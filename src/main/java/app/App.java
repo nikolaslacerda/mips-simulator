0e8c2e007d3c4bfe176dc346bfc4d7c9cfe128d8
nikolaslacerda <nikolaslacerda55@gmail.com>
2019-11-10 15:47:42 -0300

    Integrando a tela principal ao programa / Arrumando o pom

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -6,21 +6,71 @@ import controller.Executa;
 import controller.LeitorDeArquivo;
 import controller.MemoriaDeDados;
 import controller.MemoriaDeInstrucoes;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 
-public class App {
+public class App extends Application {
+
+	private double x;
+	private double y;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/views/TelaPrincipal.fxml"));
+            stage.setTitle("M.A.R.S");
+            stage.initStyle(StageStyle.UNDECORATED);
+            stage.initStyle(StageStyle.TRANSPARENT);
+            root.setOnMousePressed(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    x = event.getSceneX();
+                    y = event.getSceneY();
+                }
+            });
+            root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    stage.setX(event.getScreenX() - x);
+                    stage.setY(event.getScreenY() - y);
+                }
+            });
+
+            Scene scene = new Scene(root);
+            scene.setFill(javafx.scene.paint.Color.TRANSPARENT);
+            Image applicationIcon = new Image(getClass().getResourceAsStream("/img/mars.png"));
+            stage.getIcons().add(applicationIcon);
+            stage.centerOnScreen();
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+			System.out.println("Errooo!!" + e);
+        }
+    }
 
 	public static void main(String[] args) {
-        BancoDeRegistradores registradores = BancoDeRegistradores.getInstance();
+
+		launch(args);
+
+		BancoDeRegistradores registradores = BancoDeRegistradores.getInstance();
 		BlocoDeControle controle = BlocoDeControle.getInstance();
 		MemoriaDeInstrucoes memoriaDeInstrucoes = MemoriaDeInstrucoes.getInstance();
 		MemoriaDeDados memoriaDeDados = MemoriaDeDados.getInstance();
 		LeitorDeArquivo leitorDeArquivo = new LeitorDeArquivo("/files/mipsTest2.mips");
 
-		Executa executa= Executa.getInstance();
+		Executa executa = Executa.getInstance();
 
 		try {
-            leitorDeArquivo.lePrograma();
+			leitorDeArquivo.lePrograma();
 			memoriaDeInstrucoes.imprime();
 			//System.out.println(memoriaDeInstrucoes.getPosAtual());
 			//executa.ExecutaPrograma();
@@ -31,5 +81,5 @@ public class App {
 			e.printStackTrace();
 		}
 
-	}	
+	}
 }
